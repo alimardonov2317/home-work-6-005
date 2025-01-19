04f4a3bbe057f84c34a7f49f9320f815ec8af34a
alimardonov2317 <alimardonovsamandar707@gmail.com>
2025-01-19 14:46:22 +0500

    homework005 for the sixth month

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import "./Products.css"
+import Loading from '../loading/loading'
+
+const Products = () => {
+    const [products, setProducts] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        axios
+            .get("https://dummyjson.com/products", { params: { limit: 10 } })
+            .then(res => setProducts(res.data.products))
+            .catch(err => console.log(err))
+            .finally(() => setLoading(false))
+    }, [])
+    return (
+        <div className='container'>
+            {
+                loading && <Loading count={10} />
+            }
+            <div className="wrapper">
+                {
+                    products?.map((product) => (
+                        <div key={product.id} className="card">
+                            <img src={product.thumbnail} alt={product.title} />
+                            <h3>{product.title}</h3>
+                            <p>${product.price} USD</p>
+                            <button className="buy__btn">Buy now</button>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Products
